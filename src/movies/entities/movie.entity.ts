1974aef58571048292412ec9f66926dbaa04ea0b
rauumartinez03 <raul.us03@gmail.com>
2024-05-05 17:10:55 +0200

    Instalado Typegoose para arreglar errores de tipado

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -9,38 +9,52 @@ export class Languages extends Document{
 @Schema()
 export class Movie extends Document {
     
-    @Prop({required: true})
     name: String;
-    
-    @Prop({default: ''})
-    date: Number;
-    
-    @Prop({default: ''})
+    date: Number | ''
     tagline: String;
-    
-    @Prop({default: ''})
     description: String;
-    
-    @Prop({default: ''})
-    minute: Number;
-    
-    @Prop({default: ''})
-    rating: Number;
-    
-    @Prop({default: ['']})
-    studios: String[];
-    
-    @Prop({default: ['']})
-    genres: String[];
-    
-    @Prop({type: Languages, default: ''})
+    minute: Number | '';
+    rating: Number | '';
+    studios: [String];
+    genres: [String];
     languages: Languages | String;
-    
-    @Prop({default: ['']})
-    themes: String[];
-    
-    @Prop({default: ['']})
-    countries: String[];
+    themes: [String];
+    countries: [String];
 }
 
 export const MovieSchema = SchemaFactory.createForClass(Movie)
+
+/*
+{
+  "_id": 1000003,
+  "name": "Everything Everywhere All at Once",
+  "date": 2022,
+  "tagline": "The universe is so much bigger than you realize.",
+  "description": "An aging Chinese immigrant is swept up in an insane adventure, where she alone can save what's important to her by connecting with the lives she could have led in other universes.",
+  "minute": 140,
+  "rating": 4.32,
+  "studios": [
+    "IAC Films",
+    "TPC",
+    "Ley Line Entertainment",
+    "A24",
+    "AGBO",
+    "Year of the Rat"
+  ],
+  "genres": [ "Adventure", "Science Fiction", "Action" ],
+  "languages": {
+    "Primary language": "English",
+    "Spoken languages": [ "Cantonese", "Chinese", "English" ]
+  },
+  "themes": [
+    "Action-packed space and alien sagas",
+    "Intense combat and martial arts",
+    "Funny jokes and crude humor",
+    "Tragic sadness and captivating beauty",
+    "Quirky and endearing relationships",
+    "Relationship comedy",
+    "Epic heroes"
+  ],
+  "countries": [ "USA" ]
+}
+*/
